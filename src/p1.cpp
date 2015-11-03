8850d38fc10d435dc791ac98ac318ce0b8f3d4d8
jiangsir <jiangzero@gmail.com>
2015-11-03 17:31:22 +0800

    p1

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,5 +1,5 @@
 //============================================================================
-// Name        : 2015高雄市資訊學科能力競賽.cpp
+// Name        : p1.山洞探險
 // Author      :
 // Version     :
 // Copyright   : Your copyright notice
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "aaa" << endl; // prints
-	return 0;
+   cout << "山洞探險" << endl; // prints
+   return 0;
 }
