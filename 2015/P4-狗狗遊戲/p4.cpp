d4d9ac2e5ae4002de4306cdfd6f26b0d1c15bdb1
jiangsir <jiangzero@gmail.com>
2015-11-03 13:41:54 +0800

    結構建立。

---
+++ 2015/P4-狗狗遊戲/p4.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 2015高雄市資訊學科能力競賽.cpp
+// Author      : jiangsir
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "aaa" << endl; // prints
+	return 0;
+}
