a0a62f562cc763d4d73b0a7b32cfb2dae0199dac
jiangsir <jiangzero@gmail.com>
2015-11-03 14:55:53 +0800

    加入題目檔與測資。

--- 2015/P1-山洞探險/p1.cpp
+++ 2015/P1-山洞探險/p1.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : 2015高雄市資訊學科能力競賽.cpp
-// Author      : jiangsir
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
